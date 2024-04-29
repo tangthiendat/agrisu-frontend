150b763f65fc300a06b65f7c0f2586c6d6083b45
Tang Thien Dat <thiendattang@gmail.com>
2024-04-29 14:29:26 +0700

    feat: delete a supplier

--- src/features/suppliers/DeleteSupplier.jsx
+++ src/features/suppliers/DeleteSupplier.jsx
@@ -1,11 +1,23 @@
-import { Tooltip } from "antd";
+/* eslint-disable react/prop-types */
+import { Popconfirm, Tooltip } from "antd";
 import { MdDelete } from "react-icons/md";
+import { useDeleteSupplier } from "./hooks/useDeleteSuppliers";
 
-function DeleteSupplier() {
+function DeleteSupplier({ supplierId }) {
+  const { deleteSupplier, isDeleting } = useDeleteSupplier();
   return (
-    <Tooltip title="Xóa" placement="bottom">
-      <MdDelete className="icon" color="var(--color-red-500)" />
-    </Tooltip>
+    <Popconfirm
+      title="Xóa nhà cung cấp"
+      description="Bạn có chắc muốn xóa nhà cung cấp này không?"
+      okText="Xóa"
+      cancelText="Hủy"
+      okButtonProps={{ danger: true, loading: isDeleting }}
+      onConfirm={() => deleteSupplier(supplierId)}
+    >
+      <Tooltip title="Xóa" placement="bottom">
+        <MdDelete className="icon" color="var(--color-red-500)" />
+      </Tooltip>
+    </Popconfirm>
   );
 }
 
