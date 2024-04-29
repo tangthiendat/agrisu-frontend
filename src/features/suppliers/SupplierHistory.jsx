d30ee54b7fc0bf7b4029e60b6e01ae938868ce0f
Tang Thien Dat <thiendattang@gmail.com>
2024-04-29 17:53:21 +0700

    feat: show supplier history

--- src/features/suppliers/SupplierHistory.jsx
+++ src/features/suppliers/SupplierHistory.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-
 import { Button, Space } from "antd";
 import CreatePayment from "../payments/CreatePayment";
+import SupplierHistoryTable from "./SupplierHistoryTable";
 
-/* eslint-disable react/prop-types */
 function SupplierHistory({ supplier, onCancel }) {
   return (
     <>
+      <SupplierHistoryTable supplierId={supplier.supplierId} />
       <div className="mt-4 text-right">
         <Space>
           <Button onClick={onCancel}>Đóng</Button>
