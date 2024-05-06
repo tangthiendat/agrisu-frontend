bd7c8025cad3a90bd11348c546ecd6ea710e5bea
Tang Thien Dat <thiendattang@gmail.com>
2024-05-06 20:06:17 +0700

    refactor: handle error when fetching data

--- src/features/suppliers/hooks/useSupplierHistory.js
+++ src/features/suppliers/hooks/useSupplierHistory.js
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 import { supplierService } from "../../../services/supplierService";
 
 export function useSupplierHistory(supplierId) {
-  const { data: supplierHistory, isLoading } = useQuery({
+  const {
+    data: supplierHistory,
+    isLoading,
+    error,
+  } = useQuery({
     queryFn: () => supplierService.getHistory(supplierId),
     queryKey: ["supplierHistory", supplierId],
   });
+
+  useEffect(() => {
+    if (error) {
+      toast.error("Có lỗi xảy ra khi tải công nợ chi tiết của nhà cung cấp");
+    }
+  }, [error]);
   return { supplierHistory, isLoading };
 }
--- src/features/suppliers/hooks/useSuppliers.js
+++ src/features/suppliers/hooks/useSuppliers.js
@@ -1,10 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 import { supplierService } from "../../../services/supplierService";
 
 export function useSuppliers(page, pageSize) {
-  const { data: suppliers, isLoading: isLoadingSuppliers } = useQuery({
+  const {
+    data: suppliers,
+    isLoading: isLoadingSuppliers,
+    error,
+  } = useQuery({
     queryKey: ["suppliers", page, pageSize],
     queryFn: () => supplierService.getSuppliers(page, pageSize),
   });
+  useEffect(() => {
+    if (error) {
+      toast.error("Có lỗi xảy ra khi tải dữ liệu nhà cung cấp");
+    }
+  }, [error]);
+
   return { suppliers, isLoadingSuppliers };
 }
