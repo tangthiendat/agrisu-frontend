ee4e34751c27e015ef7e0c23b242a7924ef19bca
Tang Thien Dat <thiendattang@gmail.com>
2024-05-14 14:54:44 +0700

    refactor: improve suppliers loading
    - store pagination state in url
    - load supplier with prefetching

--- src/features/suppliers/hooks/useSuppliers.js
+++ src/features/suppliers/hooks/useSuppliers.js
@@ -1,22 +1,55 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import { supplierService } from "../../../services/supplierService";
 
-export function useSuppliers(page, pageSize) {
+export function useSuppliers() {
+  const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
+  const page = Number(searchParams.get("page")) || 1;
+  const pageSize = Number(searchParams.get("pageSize")) || 10;
+
   const {
     data: suppliers,
     isLoading: isLoadingSuppliers,
-    error,
+    error: errorSuppliers,
   } = useQuery({
-    queryKey: ["suppliers", page, pageSize],
-    queryFn: () => supplierService.getSuppliers(page, pageSize),
+    queryKey: ["suppliers", { page, pageSize }],
+    queryFn: () => supplierService.getSuppliers({ page, pageSize }),
   });
+
+  const {
+    data: suppliersNum,
+    isLoading: isLoadingSuppliersNum,
+    error: errorSuppliersNum,
+  } = useQuery({
+    queryKey: ["suppliersNum"],
+    queryFn: supplierService.count,
+  });
+
+  const isLoading = isLoadingSuppliers || isLoadingSuppliersNum;
+  const error = errorSuppliers || errorSuppliersNum;
+
   useEffect(() => {
     if (error) {
       toast.error("Có lỗi xảy ra khi tải dữ liệu nhà cung cấp");
     }
   }, [error]);
 
-  return { suppliers, isLoadingSuppliers };
+  const pageCount = Math.ceil(suppliersNum / pageSize);
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["suppliers", { page: page + 1, pageSize }],
+      queryFn: () => supplierService.getSuppliers({ page: page + 1, pageSize }),
+    });
+  }
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["suppliers", { page: page - 1, pageSize }],
+      queryFn: () => supplierService.getSuppliers({ page: page - 1, pageSize }),
+    });
+  }
+
+  return { suppliers, isLoading, suppliersNum };
 }
